a8ebee3ee928da207a07fc49c463a9585e0e7b3c
tomicakr <tomislav.kravarscan@productive.io>
2018-10-04 15:19:50 +0200

    ajax request with xmlhttprequest object

--- 05.AJAX_Request/src/ajaxRequest.js
+++ 05.AJAX_Request/src/ajaxRequest.js
@@ -1,4 +1,25 @@
-function ajaxReq() {
+function ajaxReq(url, options) {
   'use strict';
   // code goes here
+
+  const req = new XMLHttpRequest();
+  const { method, data } = options;
+
+  const onSuccess = options.success.bind(options.context);
+  const onFailure = options.failure.bind(options.context);
+  const onComplete = options.complete.bind(options.context);
+
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.open(method, url, true);
+
+  req.onload = function (){
+    if (this.status === 200) {
+      onSuccess(JSON.parse(this.responseText), this.status, this);
+    } else {
+      onFailure(this, this.status, this.responseText);
+    }
+    onComplete(this, this.status);
+  }
+  
+  req.send(JSON.stringify(data));
 }
