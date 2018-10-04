995aa26b759d60ac0b40c40f715b2f4ba58ac958
tomicakr <tomislav.kravarscan@productive.io>
2018-10-04 12:04:59 +0200

    css class manipulation: add, remove, toggle, has

--- 03.CSS_class_manipulation/src/cssClassManipulation.js
+++ 03.CSS_class_manipulation/src/cssClassManipulation.js
@@ -2,4 +2,27 @@ var cssClass = (function() {
   'use strict';
 
   // code goes here
+
+  return {
+    add,
+    remove,
+    toggle,
+    has
+  }
 })();
+
+function add(element, className) {
+  element.classList.add(className);
+}
+
+function remove(element, className) {
+  element.classList.remove(className);
+}
+
+function toggle(element, className) {
+  element.classList.toggle(className);
+}
+
+function has(element, className) {
+  return element.classList.contains(className);
+}
