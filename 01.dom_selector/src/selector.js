1f0e354e5c49a47ee7b57b4db8fff7227d4b4923
tomicakr <tomislav.kravarscan@productive.io>
2018-10-04 10:50:28 +0200

    dom selector with querySelector method and css manipulation with style attribute on elements

--- 01.dom_selector/src/selector.js
+++ 01.dom_selector/src/selector.js
@@ -2,4 +2,5 @@ var domSelector = function(selectors) {
   'use strict';
 
   //code goes here
+  return Array.from(document.querySelectorAll(selectors));
 };
--- 02.CSS_manipulation/src/cssProp.js
+++ 02.CSS_manipulation/src/cssProp.js
@@ -1,5 +1,19 @@
-function cssProp() {
+function cssProp(element, cssProperty, value) {
   'use strict';
 
   //code goes here
+
+  if(typeof cssProperty === 'object') {
+    for (const attr in cssProperty) {
+      element.style[attr] = cssProperty[attr];
+    }
+  }
+
+  else if(typeof value === 'undefined') {
+    return element.style[cssProperty];
+  }
+
+  else {
+    element.style[cssProperty] = value;
+  }
 }
