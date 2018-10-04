2fcba6f2e7bba5b36e97efecbdb8a2f5ba6e6b2f
tomicakr <tomislav.kravarscan@productive.io>
2018-10-04 12:33:32 +0200

    dom manipulation, mainly using insertAdjacentElement method

--- 04.DOM_manipulation/src/domManipulation.js
+++ 04.DOM_manipulation/src/domManipulation.js
@@ -1,3 +1,38 @@
-var dom = function(){
+var dom = (function(){
+
+  'use strict'
   // code goes here
-};
+
+  return {
+    remove,
+    append, 
+    after,
+    prepend,
+    before,
+    val
+  }
+})();
+
+function val(element) {
+  return element.value;
+}
+
+function remove(element) {
+  element.remove();
+}
+
+function append(targetElement, element) {
+  targetElement.insertAdjacentElement('beforeend', element);
+}
+
+function after(targetElement, element) {
+  targetElement.insertAdjacentElement('afterend', element);
+}
+
+function prepend(targetElement, element) {
+  targetElement.insertAdjacentElement('afterbegin', element);
+}
+
+function before(targetElement, element) {
+  targetElement.insertAdjacentElement('beforebegin', element);
+}
